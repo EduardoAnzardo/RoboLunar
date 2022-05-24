3c0f4cf0514d38cd773f85739d3e180d5250faba
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2022-05-24 15:05:45 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    Robo R = new Robo();
+    R.Bateria = 100;
+    R.Passos = 0;
+    R.Foto = 0;
+    R.Status();
+    System.out.println(R);
+  }
+}
